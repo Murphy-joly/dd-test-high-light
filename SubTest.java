8cdeb326b5717305931277021cebb4d560b21913
zhoudandan10 <zhoudandan10@meituan.com>
2024-12-03 10:15:41 +0800

    add comment

--- SubTest.java
+++ SubTest.java
@@ -1,10 +1,13 @@
 package org.springframework.messaging;
 
+/**多行注释1
+ * 多行注释2
+ */
 public class SubTest{
+    //我是一个单行注释
     private final String name = "hello mei tuan";
     @Override
     public String toString(){
-
         return super.toString();
     }
 }
