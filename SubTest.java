8bbe35ba08d467f57784d9f19136cbb4b0b1d827
zhoudandan10 <zhoudandan10@meituan.com>
2024-12-03 09:31:53 +0800

    add constant

--- SubTest.java
+++ SubTest.java
@@ -1,7 +1,9 @@
 package org.springframework.messaging;
 
 public class SubTest{
+    private final String name = "hello mei tuan";
     public String toString(){
+
         return super.toString();
     }
 }
