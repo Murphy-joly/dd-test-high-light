d2e193601c0941d46783f148ec6367577217f3c2
zhoudandan10 <zhoudandan10@meituan.com>
2024-12-03 09:36:57 +0800

    1

--- SubTest.java
+++ SubTest.java
@@ -2,6 +2,7 @@ package org.springframework.messaging;
 
 public class SubTest{
     private final String name = "hello mei tuan";
+    @Override
     public String toString(){
 
         return super.toString();
