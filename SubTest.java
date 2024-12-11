f9957dd661accd78f4f2a1850dcf297d472f6625
zhoudandan10 <zhoudandan10@meituan.com>
2024-12-11 16:02:28 +0800

    1

--- SubTest.java
+++ SubTest.java
@@ -7,7 +7,7 @@ public class SubTest{
     //我是一个单行注释
     private final String name = "hello mei tuan";
 
-    private final int numChange = 1234567;
+    private static final int numChange = 1234567;
     private final Integer num2 = 1234567;
     @Override
     public String toString(){
