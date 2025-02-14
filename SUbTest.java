fdb191c417d3740c7dd7a64c0d889d10befb7aab
zhoudandan10 <zhoudandan10@meituan.com>
2025-02-14 14:51:37 +0800

    1

---
+++ SUbTest.java
@@ -0,0 +1,13 @@
+package org.springframework.messaging;
+
+public class SubTest{
+    //我是一个单行注释
+    private final String name = "hello mei tuan";
+
+    private final int num = 1234567;
+    private final Integer num2 = 1234567;
+    @Override
+    public String toString(){
+        return super.toString();
+    }
+}
